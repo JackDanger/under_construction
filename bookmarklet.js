46fe59bc0f4db0aeffc6ff91107d2eb360f5242a
Jack Danger Canty <gitcommit@6brand.com>
2009-12-02 15:33:57 -0800

    adding bookmarklet

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+// "Production" mode (HA!)
+// javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://jackdanger.github.com/under_construction/break_everything.js';})();
+
+// Development
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://cloops.local/under_construction/scripts/mouse_trails_clean.js';})();
